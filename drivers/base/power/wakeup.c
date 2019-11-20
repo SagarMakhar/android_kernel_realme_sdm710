181ffac34aab9bdb66eeb1294eddc61bf622f3a1
SagarMakhar <sagarmakhar@gmail.com>
2019-11-20 11:24:03 +0000

    drivers: base: Import realme changes
    * Add for Sync App and Kernel time
      Add interface to get proper fw
    
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -17,6 +17,9 @@
 #include <linux/pm_wakeirq.h>
 #include <linux/types.h>
 #include <trace/events/power.h>
+#ifdef CONFIG_PRODUCT_REALME_RMX1901
+#include <linux/rtc.h>
+#endif /* CONFIG_PRODUCT_REALME_RMX1901 */
 #include <linux/irq.h>
 #include <linux/irqdesc.h>
 
@@ -1100,12 +1103,61 @@ static int wakeup_sources_stats_open(struct inode *inode, struct file *file)
 	return single_open(file, wakeup_sources_stats_show, NULL);
 }
 
+#ifdef CONFIG_PRODUCT_REALME_RMX1901
+static ssize_t watchdog_write(struct file *file, const char __user *buf, size_t count, loff_t *ppos)
+{
+	s32 value;
+	struct timespec ts;
+	struct rtc_time tm;
+
+	if (count == sizeof(s32)) {
+		if (copy_from_user(&value, buf, sizeof(s32)))
+			return -EFAULT;
+	} else if (count <= 11) { /* ASCII perhaps? */
+		char ascii_value[11];
+		unsigned long int ulval;
+		int ret;
+
+		if (copy_from_user(ascii_value, buf, count))
+			return -EFAULT;
+
+		if (count > 10) {
+			if (ascii_value[10] == '\n')
+				ascii_value[10] = '\0';
+			else
+				return -EINVAL;
+		} else {
+			ascii_value[count] = '\0';
+		}
+		ret = kstrtoul(ascii_value, 16, &ulval);
+		if (ret) {
+			pr_debug("%s, 0x%lx, 0x%x\n", ascii_value, ulval, ret);
+			return -EINVAL;
+		}
+		value = (s32)lower_32_bits(ulval);
+	} else {
+		return -EINVAL;
+	}
+
+	getnstimeofday(&ts);
+	rtc_time_to_tm(ts.tv_sec, &tm);
+	pr_warn("!@WatchDog_%d; %d-%02d-%02d %02d:%02d:%02d.%09lu UTC\n",
+		value, tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
+		tm.tm_hour, tm.tm_min, tm.tm_sec, ts.tv_nsec);
+
+	return count;
+}
+#endif /* CONFIG_PRODUCT_REALME_RMX1901 */
+
 static const struct file_operations wakeup_sources_stats_fops = {
 	.owner = THIS_MODULE,
 	.open = wakeup_sources_stats_open,
 	.read = seq_read,
 	.llseek = seq_lseek,
 	.release = single_release,
+#ifdef CONFIG_PRODUCT_REALME_RMX1901
+	.write          = watchdog_write,
+#endif /* CONFIG_PRODUCT_REALME_RMX1901 */
 };
 
 static int __init wakeup_sources_debugfs_init(void)
