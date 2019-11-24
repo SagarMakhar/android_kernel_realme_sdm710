28c995b841245cbc7ad56ec3664b514de4064dd6
SagarMakhar <sagarmakhar@gmail.com>
2019-11-24 05:14:13 +0000

    ASoC: Fix extraneous parantheses warning
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- techpack/audio/asoc/sdm660-common.c
+++ techpack/audio/asoc/sdm660-common.c
@@ -3626,7 +3626,7 @@ static int msm_asoc_machine_probe(struct platform_device *pdev)
 #endif /* CONFIG_PRODUCT_REALME_RMX1901 */
 
 #ifdef CONFIG_PRODUCT_REALME_RMX1901
-	if ((pdata->snd_card_val == INT_SND_CARD)) {
+	if (pdata->snd_card_val == INT_SND_CARD) {
 		if (msm_cdc_pinctrl_select_sleep_state(pdata->dmic_gpio_p)) {
 			pr_err("%s: set dmic data pin high-z state error\n", __func__);
 		}
